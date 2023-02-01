5a214a137d6abae2801f5bf4023c667f0a442d0b
omejia1997 <omardevaluado08@gmail.com>
2023-01-31 20:16:09 -0500

    cambios en pom

--- src/main/java/ec/edu/espe/gpr/model/Tarea.java
+++ src/main/java/ec/edu/espe/gpr/model/Tarea.java
@@ -58,7 +58,7 @@ public class Tarea implements Serializable {
 
     @Column(name = "FECHA_CREACIONTAREA")
     @Temporal(TemporalType.TIMESTAMP)
-    @JsonFormat(shape = JsonFormat.Shape.STRING ,pattern = "dd-MM-YYYY HH:mm:ss" , timezone="UTC") 
+    @JsonFormat(shape = JsonFormat.Shape.STRING ,pattern = "yyyy-MM-dd HH:mm:ss" , timezone="UTC") 
     private Date fechaCreaciontarea;
     
     @Column(name = "PRIORIDAD_TAREA")
